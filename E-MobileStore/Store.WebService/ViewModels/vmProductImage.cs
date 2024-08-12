23df8dca6efb83eaa327d93029f7389880569478
NguyenThuanPhat <thuanphatnguyen2003@gmail.com>
2024-08-12 23:51:54 +0700

    call api

---
+++ E-MobileStore/Store.WebService/ViewModels/vmProductImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.WebService.ViewModels
+{
+    public class vmProductImage
+    {
+        public Guid Id { get; set; }
+        public string ImageURL { get; set; } = string.Empty;
+        public string ImageName { get; set; } = string.Empty;
+        public string Position { get; set; } = string.Empty;
+    }
+}
